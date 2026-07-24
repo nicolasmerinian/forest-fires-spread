cfa39d1bc5345d9b1c1e78a65a335ecac7c89d48
Nicolas Merinian <nicolas.merinian@gmail.com>
2026-07-25 00:56:22 +0200

    tech: added a parameter 'simulationSpeed'

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 import FF from "./ff.js";
 
-const ff = new FF(100, 7, 5, 0.45, 0);
+const rowAndColNumber = 200; // default : 100
+const cellSize = 3; // default : 7
+const p = 0.55;  // default and threshold: 0.55
+const firemenNumber = 0;
+const simulationSpeed = 10; // default : 100
+
+const ff = new FF(rowAndColNumber, cellSize, p, firemenNumber, simulationSpeed);
