8cb2eb965e6f976bc5d676660e3c564ec3de3751
Nicolas Merinian <nicolas.merinian@gmail.com>
2026-08-05 16:06:04 +0200

    feat: added mixed forest scenario

--- src/app.js
+++ src/app.js
@@ -3,10 +3,11 @@ import ForestFireRenderer from "./renderer/ForestFireRenderer.js";
 import defaults from "./config/defaults.js";
 import buildConfig from "./config/buildConfig.js";
 import pineForest from "./data/scenarios/pineForest.js";
+import mixedForest from "./data/scenarios/mixedForest.js";
 import vegetationTypes from "./data/vegetationTypes.js";
 import directionVectors from "./constants/directions.js";
 
-const modelConfig = buildConfig(defaults, pineForest, vegetationTypes, directionVectors);
+const modelConfig = buildConfig(defaults, mixedForest, vegetationTypes, directionVectors);
 
 const model = new ForestFireModel(modelConfig);
 
---
+++ src/data/scenarios/mixedForest.js
@@ -0,0 +1,8 @@
+export default {
+    composition: {
+        OAK: 0.4,
+        BEECH: 0.3,
+        SHRUB: 0.2,
+        GRASS: 0.1
+    }
+};
